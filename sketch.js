9f213abb97b37092f88293a7cdcc3252a9af8004
Bassel Bitar <91824524+basselbitar@users.noreply.github.com>
2025-08-26 15:38:00 +0100

    exploring how to draw and rotate objects

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
   createCanvas(1024, 1024);
+
+  // Create screen reader accessible description
+  describe('moving shapes of squares, circles and triangles');
 }
 
 function draw() {
-  background(220);
+  background("aqua");
+  if (mouseIsPressed === true) {
+  fill(0);
+} else {
+  fill(255);
+}
+
+  translate(width/2, height/2);
+  rotate(frameCount/100);
+  stroke(255);
+  strokeWeight(15);
+  line(0, 0, 150, 0);
+  circle(140, 0, 50);
 }
