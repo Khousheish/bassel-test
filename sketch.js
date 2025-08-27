ff09f5787ab86ff74c62102876351825c01ed409
Bassel Bitar <91824524+basselbitar@users.noreply.github.com>
2025-08-27 08:59:57 +0100

    Circles class that move randomly

--- sketch.js
+++ sketch.js
@@ -1,22 +1,58 @@
+let circles = [];
+
 function setup() {
-  createCanvas(1024, 1024);
+  createCanvas(800, 800);
 
   // Create screen reader accessible description
-  describe('moving shapes of squares, circles and triangles');
+  describe("moving shapes of squares, circles and triangles");
+
+  for (let i = 0; i < 20; i++) {
+    let c1 = new Circle(random(width), random(height), random(10, 100));
+    circles.push(c1);
+  }
 }
 
 function draw() {
   background("aqua");
-  if (mouseIsPressed === true) {
-  fill(0);
-} else {
-  fill(255);
+  circles.forEach(c => {
+
+
+      let speed = random(1, 5);
+    c.move(speed, width, height);
+
+
+    circle(c.posX, c.posY, c.diameter);
+  });
 }
 
-  translate(width/2, height/2);
-  rotate(frameCount/100);
-  stroke(255);
-  strokeWeight(15);
-  line(0, 0, 150, 0);
-  circle(140, 0, 50);
+
+class Circle {
+    constructor(posX, posY, diameter) {
+        this.posX = posX;
+        this.posY = posY;
+        this.diameter = diameter;
+        this.direction = random(2*Math.PI);
+    }
+
+    move(speed, canvasWidth, canvasHeight) {
+      //randomly move each circle
+      this.posX += cos(this.direction) * speed;
+      this.posY += sin(this.direction) * speed;
+
+      if(this.posX + this.diameter/2 >= canvasWidth) {
+        this.direction = Math.PI - this.direction;
+        this.posX -= 1;
+      } else if(this.posX - this.diameter/2 <= 0) {
+        this.direction = Math.PI - this.direction;
+        this.posX += 1;
+      }
+
+      if(this.posY + this.diameter/2 >= canvasHeight) {
+        this.direction = -this.direction;
+        this.posY -= 1;
+      } else if(this.posY - this.diameter/2 <= 0) {
+        this.direction = -this.direction;
+        this.posY += 1;
+      }
+    }
 }
